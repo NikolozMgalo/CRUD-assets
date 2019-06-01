83e65b72993795643e419eb0c16de9c8b0d3a0fb
nikolozmgalo <nikolozmgalo@gmail.com>
2019-06-01 22:55:44 +0400

    fix errors, prettier

--- handler/handler.js
+++ handler/handler.js
@@ -1,76 +1,70 @@
-const Promise = require("bluebird")
-const mongoose = require('mongoose')
-const Asset = require('../model/asset.model.js')
+const Promise = require('bluebird');
+const mongoose = require('mongoose');
+const Asset = require('../model/asset.model.js');
 
+module.exports = {
+  findAll(req, h) {
+    return new Promise((resolve, reject) => {
+      Asset.find({}, (err, assets) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(assets);
+      });
+    });
+  },
 
-module.exports = 
-{
-	findAll(req, h)
-	{
-		return new Promise((resolve, reject) => {
-			Asset.find({}, (err, assets) =>{
-				if(err){
-					reject(err)
-				} 
-				resolve(assets)
-			})
-		})
-		
+  findOne(req, h) {
+    return new Promise((resolve, reject) => {
+      Asset.find({ _id: req.params.id }, (err, asst) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(asst);
+      });
+    });
+  },
 
-	},
-
-	findOne(req, h)
-	{
-		return new Promise((resolve, reject) => {
-			Asset.find({_id: req.params.id}, (err, asst) => {
-				if(err){
-					reject(err)
-				}
-				resolve(asst)
-			})
-		})
-	},
-
-	deleteOne(req, h)
-	{
-		return Asset.findOneAndDelte({_id: req.params.id}).then((asset) =>{
-			return {message: "deleted"}
-		}).catch(err => {
-			return {err: "error has occured double check infrot"}
-		})
-	},
-	createNew(req, h)
-	{
-		return new Promise((resolve, reject) => {
-			Asset.create({
-				name: req.payload.name,
-				price: req.payload.price
-			}, (err, asst) => {
-				if(err){
-					reject(err => {
-						return {err: " Error has occured please double check information"}
-					})
-				}
-				resolve(asst)
-			})
-		})
-		
-	},
-	updateOne(req, h)
-	{	
-		let assetData = {}
-		return Asset.findById({_id : req.params.id}).then((asset) =>{ 
-			asset.name = req.payload.name,
-			asset.price = req.payload.price
-			asset.save(assetData)
-		}).then((data) =>{
-			return {message: "updated"}
-		}).catch(err => {
-			
-			return {err: "error has occured, please double check the ID"}
-		})
-
-	
-	}
-
-}
+  deleteOne(req, h) {
+    return new Promise((resolve, reject) => {
+      Asset.findOneAndDelte({ _id: req.params.id });
+      if (err) {
+        reject(err);
+      }
+      resolve(asst);
+    });
+  },
+  createNew(req, h) {
+    return new Promise((resolve, reject) => {
+      Asset.create(
+        {
+          name: req.payload.name,
+          price: req.payload.price
+        },
+        (err, asst) => {
+          if (err) {
+            reject(err => {
+              return {
+                err: ' Error has occured please double check information'
+              };
+            });
+          }
+          resolve(asst);
+        }
+      );
+    });
+  },
+  updateOne(req, h) {
+    let assetData = {};
+    return new Promise((resolve, reject) => {
+      Asset.findById({ _id: req.params.id }).then(asset => {
+        (asset.name = req.payload.name), (asset.price = req.payload.price);
+        asset.save(assetData);
+      });
+      if (err) {
+        reject(err);
+      }
+      resolve(asst);
+    });
+  }
+};
--- routes/routes.js
+++ routes/routes.js
@@ -1,53 +1,51 @@
-const assetHandler = require('../handler/handler.js')
-const Joi = require('joi')
+const assetHandler = require('../handler/handler.js');
+const Joi = require('joi');
 
-module.exports = 
-[
-	{
-		method: 'GET',
-		path: '/assets',
-		handler: assetHandler.findAll
-	},
-	{
-		method: 'GET',
-		path: '/assets/{id}',
-		handler: assetHandler.findOne,
-		options: {
-			validate:{
-				params: {
-					id: Joi.required()
-				}
-			}
-		}
-	},
-	{
-		method: 'POST',
-		path: '/assets',
-		handler: assetHandler.createNew
-	},
-	{
-		method: 'PUT',
-		path: '/assets/{id}',
-		handler: assetHandler.updateOne,
-		options:{
-			validate:{
-				params:{
-					id: Joi.required()
-				}
-			}
-		}
-	},
-	{
-		method: 'DELETE',
-		path: '/assets/{id}',
-		handler: assetHandler.deleteOne,
-		options:{
-			validate:{
-				params:{
-					id: Joi.required()
-				}
-			}
-		}
-	},
-
-]
+module.exports = [
+  {
+    method: 'GET',
+    path: '/assets',
+    handler: assetHandler.findAll
+  },
+  {
+    method: 'GET',
+    path: '/assets/{id}',
+    handler: assetHandler.findOne,
+    options: {
+      validate: {
+        params: {
+          id: Joi.required()
+        }
+      }
+    }
+  },
+  {
+    method: 'POST',
+    path: '/assets',
+    handler: assetHandler.createNew
+  },
+  {
+    method: 'PUT',
+    path: '/assets/{id}',
+    handler: assetHandler.updateOne,
+    options: {
+      validate: {
+        params: {
+          id: Joi.required()
+        }
+      }
+    }
+  },
+  {
+    method: 'DELETE',
+    path: '/assets/{id}',
+    handler: assetHandler.deleteOne,
+    options: {
+      validate: {
+        params: {
+          id: Joi.required()
+        }
+      }
+    }
+  }
+];
